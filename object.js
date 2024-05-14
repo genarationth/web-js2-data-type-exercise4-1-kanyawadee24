ea8ec969ca351536b110b2ab4eed1702b820de43
Kanyawadee kongkaew <kanyawadeekongkaew@partykywdd.local>
2024-05-14 12:11:35 +0700

    js2-ex4-1

---
+++ object.js
@@ -0,0 +1,16 @@
+const products = {
+  product_id: "",
+  product_name: "",
+  quantity_in_stoc: "",
+  unit_price: "",
+  product_description: {
+    product_brand : "",
+    size_or_quantity : "",
+    taste_or_scent : "",
+    color : "" ,
+    addition_description : ""
+}
+};
+
+console.log(products.product_description[1]);
+console.log(products[3]);
